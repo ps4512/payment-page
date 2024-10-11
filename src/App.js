b4104f6af3b31639dc14620c1ee16ff8a326eafa
ps4512 Shao <simonbook123@googlemail.com>
2024-10-11 10:26:29 +0000

    it works without actions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AdyenCheckout, Dropin } from '@adyen/adyen-web';
+import { AdyenCheckout, Dropin, Card } from '@adyen/adyen-web';
 import '@adyen/adyen-web/styles/adyen.css';
 
 
 const Checkout = () => {
 
   const [paymentMethodsResponse, setPaymentMethodsResponse] = useState(null);
+  const AMOUNT = {
+    value: 1000,
+    currency: 'EUR',
+  };
+  const LOCALE = 'zh-CN';
+  const COUNTRYCODE= 'NL';
 
   useEffect(() => {
     const postData = async () => {
       try {
-        const response = await fetch('https://ps4512-paymentserver-qw5gp71ih8u.ws-us116.gitpod.io/payment-methods', {
+        const response = await fetch('http://localhost:5002/payment-methods', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
         });
         const data = await response.json();
-        console.log(data);
         setPaymentMethodsResponse(data); // Update state with fetched data
       } catch (error) {
         console.error('Error:', error);
@@ -34,15 +39,68 @@ const Checkout = () => {
       const configuration = {
         clientKey: "test_HYAHXGJMHRBMVP3MBLIZEZ4UFA6KRRPW",
         environment: "test",
-        amount: {
-          value: 1000,
-          currency: 'EUR',
-        },
-        locale: 'nl-NL',
-        countryCode: 'NL',
+        amount: AMOUNT,
+        locale: LOCALE,
+        countryCode: COUNTRYCODE,
         paymentMethodsResponse: paymentMethodsResponse,
         onSubmit: async (state, component, actions) => {
           // Handle submission
+         async function makePaymentsCall(paymentdata, amount) {
+            try {
+              const response = await fetch('http://localhost:5002/payments', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(
+                  {
+                    amount,
+                    reference: "Your order number",
+                    paymentMethod: paymentdata.paymentMethod,
+                    returnUrl: "https://your-company.com/...",
+                    merchantAccount: "AdyenTechSupport_PengShao_TEST"
+                   }
+                ),
+
+              });
+              const data = await response.json();
+              return data;
+            } catch (error) {
+              console.error('Error:', error);
+            }
+          }
+
+          try {
+            // Make a POST /payments request from your server.
+            const result = await makePaymentsCall(state.data, AMOUNT);
+
+            console.log("result code is: " + result.resultCode);
+       
+            // If the /payments request from your server fails, or if an unexpected error occurs.
+            if (!result.resultCode) {
+              actions.reject();
+              return;
+            }
+       
+            const {
+              resultCode,
+              action,
+              order,
+              donationToken
+            } = result;
+       
+            // If the /payments request request form your server is successful, you must call this to resolve whichever of the listed objects are available.
+            // You must call this, even if the result of the payment is unsuccessful.
+            actions.resolve({
+              resultCode,
+              action,
+              order,
+              donationToken,
+            });
+          } catch (error) {
+            console.error("onSubmit", error);
+            actions.reject();
+          }
         },
         onAdditionalDetails: async (state, component, actions) => {
           // Handle additional details
@@ -57,18 +115,31 @@ const Checkout = () => {
           console.error(error.name, error.message, error.stack, component);
         },
       };
-  
-      const checkout = AdyenCheckout(configuration);
+
+      async function asyncCall() {
+        const checkout = await AdyenCheckout(configuration);
       
-      // Ensure to mount only when the container exists
-      const dropinContainer = document.getElementById('dropin-container');
-      if (dropinContainer) {
-        new Dropin(checkout).mount(dropinContainer);
-      } else {
-        console.error("Drop-in container not found.");
+        // Ensure to mount only when the container exists
+        const dropinContainer = document.getElementById('dropin-container');
+        if (dropinContainer) {
+          // Create an instance of Drop-in.
+          const dropin = new Dropin(checkout, {
+          // Include the payment methods that imported.
+          paymentMethodComponents: [Card],
+          // Mount it to the container you created.
+          }).mount(dropinContainer);        
+        } 
+        else {
+          console.error("Drop-in container not found.");
+        }
       }
+
+      asyncCall()
+
     }
   }, [paymentMethodsResponse]);
+
+  return <div id="dropin-container"></div>;
 }
 
 function App() {
